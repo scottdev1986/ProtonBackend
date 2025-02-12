1ce0f249aeb0be2639f906db46af204bfc618e5f
Scott Kellar <devscott1986@gmail.com>
2025-02-12 11:26:14 -0500

    add authentication to application

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, Security
 from app.models.hello import Hello
-
+from app.utils.auth import VerifyToken
 
 router = APIRouter()
+auth = VerifyToken()
 
 
 @router.get("/")
-async def hello(response: Response):
+async def hello(response: Response, auth_result: str = Security(auth.verify)):
     m = Hello(name="John", age=25, city="New York")
-    return {"user": m}
+    return {"user": m, "auth_result": auth_result}
