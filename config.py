79fa86496557ee70ad930ae87b72076cbca540ac
Scott Kellar <devscott1986@gmail.com>
2025-02-12 13:04:51 -0500

    add tests for auth

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    auth0_domain: str = None
-    auth0_api_audience: str = None
-    auth0_issuer: str = None
-    auth0_algorithms: str = None
+    auth0_domain: str = "test.auth0.com"
+    auth0_api_audience: str = "https://test.auth0.com/api/v2/"
+    auth0_issuer: str = "https://test.auth0.com/"
+    auth0_algorithms: str = "RS256"
 
 
 settings = Settings()
